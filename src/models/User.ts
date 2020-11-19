1b992c5f8be47bdf448d299b00d7f4c2599fb2f6
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-11-19 11:22:54 +0100

    wire up attributes to User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,39 +1,20 @@
-import axios, { AxiosResponse } from "axios";
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
-interface UserProps {
+import { Sync } from "./Sync";
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = "http://localhost:3000/users";
+
 export class User {
-  id = this.get("id");
-
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/${this.id}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    if (this.id) {
-      axios.put(`http://localhost:3000/users/${this.id}`, this.data);
-    } else {
-      axios.post("http://localhost:3000/users", this.data);
-    }
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
