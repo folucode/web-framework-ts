f6786fb062f2a415b85a247d95b55532898e700f
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-11-19 14:05:20 +0100

    refactor Model class with shortened syntax

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -132,29 +132,10 @@ function () {
     this.attributes = attributes;
     this.events = events;
     this.sync = sync;
+    this.on = this.events.on;
+    this.trigger = this.events.trigger;
+    this.get = this.attributes.get;
   }
-
-  Object.defineProperty(Model.prototype, "on", {
-    get: function get() {
-      return this.events.on;
-    },
-    enumerable: false,
-    configurable: true
-  });
-  Object.defineProperty(Model.prototype, "trigger", {
-    get: function get() {
-      return this.events.trigger;
-    },
-    enumerable: false,
-    configurable: true
-  });
-  Object.defineProperty(Model.prototype, "get", {
-    get: function get() {
-      return this.attributes.get;
-    },
-    enumerable: false,
-    configurable: true
-  });
 
   Model.prototype.set = function (update) {
     this.attributes.set(update);
@@ -2157,7 +2138,6 @@ Object.defineProperty(exports, "__esModule", {
 var User_1 = require("./models/User");
 
 var user = User_1.User.buildUser({
-  id: 1,
   name: "Tosin Moronfolu",
   age: 23
 });
