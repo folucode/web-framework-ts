c9c786b809f56b4dd195dded315bb6ac423e6c96
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-11-26 13:58:38 +0100

    Implement collection view

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
-import { UserForm } from "./views/UserForm";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = User.buildUser({ name: "Tosin", age: 23 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById("root");
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error("Root Element not found");
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
